a56d227151a9d74b283237ba0a1a3a63ec1720fd
PRITISH TOMAR <pritishtomar2004@gmail.com>
2024-07-19 00:55:36 +0530

    Commit for Phase 1

---
+++ dist/dist/dist/index.js
@@ -0,0 +1,37 @@
+// import Connect from './Utils/Connect.js';
+import express from 'express';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+import cors from "cors";
+import Routemain from './Routes/Routemain.js';
+import RouteArticles from './Routes/RouteArticles.js';
+import RouteComment from './Routes/RouteComment.js';
+import Email from "./Routes/RouteEmail.js";
+
+// Connect()
+dotenv.config({
+  path: '.env'
+});
+mongoose.connect(process.env.MONGO_URL).then(() => {
+  console.log("Mongodb connected");
+}).catch(e => console.log(e));
+
+//..................................................................
+
+const app = express();
+app.use(express.urlencoded({
+  extended: true
+}));
+app.use(express.json());
+const corsOptions = {
+  origin: process.env.ORIGIN,
+  credentials: true
+};
+app.use(cors(corsOptions));
+app.use("/api", Routemain);
+app.use("/articles", RouteArticles);
+app.use("/comments", RouteComment);
+app.use("/email", Email);
+app.listen(process.env.PORT, () => {
+  console.log('Server Running at Port ' + process.env.PORT);
+});
